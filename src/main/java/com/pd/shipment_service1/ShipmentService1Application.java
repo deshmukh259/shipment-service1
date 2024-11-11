854f4d9524950ee97b256ca40a8a89edea22e3e8
purushottam <deshmukh259.2020@gmail.com>
2024-11-11 10:51:14 +0530

    shipment service

---
+++ src/main/java/com/pd/shipment_service1/ShipmentService1Application.java
@@ -0,0 +1,13 @@
+package com.pd.shipment_service1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShipmentService1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShipmentService1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/pd/shipment_service1/ShipmentService1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pd.shipment_service1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShipmentService1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
